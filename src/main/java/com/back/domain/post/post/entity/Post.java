2b1696c4667f36dc1d7314591c4573b56c870839
jhs512 <jangka512@gmail.com>
2026-05-04 15:35:45 +0900

    022

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -15,17 +15,21 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private int id;
+    private final int id;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
+        this.id = 0;
         this.title = title;
         this.content = content;
+    }
+
+    public Post() {
+        this(null, null);
     }
 }
