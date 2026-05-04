da7be2498b95dad11ff1fe305e4c18c172fd5c39
jhs512 <jangka512@gmail.com>
2026-05-04 15:16:45 +0900

    017

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,5 +17,6 @@ public class Post {
     @GeneratedValue(strategy = IDENTITY)
     private int id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String content;
 }
