830fb1bbcff09db7d75b3d6689daf1771c1b80c2
jhs512 <jangka512@gmail.com>
2026-05-04 15:42:09 +0900

    025

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,7 +1,7 @@
 package com.back.global.initData;
 
 import com.back.domain.post.post.entity.Post;
-import com.back.domain.post.post.repository.PostRepository;
+import com.back.domain.post.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Configuration
 public class BaseInitData {
     @Autowired
-    private PostRepository postRepository;
+    private PostService postService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
@@ -23,11 +23,11 @@ public class BaseInitData {
     }
 
     void work1() {
-        if (postRepository.count() > 0) return;
+        if (postService.count() > 0) return;
 
         Post post1 = new Post("제목 1", "내용 1");
-        postRepository.save(post1);
-        Post post2 = postRepository.save(new Post("제목 1", "내용 2"));
+        postService.save(post1);
+        Post post2 = postService.save(new Post("제목 1", "내용 2"));
 
         System.out.println(post1.getId());
         System.out.println(post2.getId());
@@ -36,7 +36,7 @@ public class BaseInitData {
     }
 
     void work2() {
-        Optional<Post> opPost1 = postRepository.findById(1);
+        Optional<Post> opPost1 = postService.findById(1);
         Post post1 = opPost1.get();
 
         System.out.println("post1 : " + post1);
