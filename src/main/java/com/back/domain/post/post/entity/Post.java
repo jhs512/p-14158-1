0791aae044dcb7687c3cd6a0983ebc93d3327206
jhs512 <jangka512@gmail.com>
2026-05-04 15:38:47 +0900

    024

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -20,11 +22,15 @@ public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = this.createDate;
         this.title = title;
         this.content = content;
     }
