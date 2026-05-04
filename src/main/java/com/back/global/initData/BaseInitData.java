7ea6588366794f6512557f113cc1e1cbdd69956a
jhs512 <jangka512@gmail.com>
2026-05-04 15:51:12 +0900

    029

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -2,6 +2,7 @@ package com.back.global.initData;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Optional;
 
 @Configuration
+@AllArgsConstructor
 public class BaseInitData {
     private PostService postService;
-
-    public BaseInitData(PostService postService) {
-        this.postService = postService;
-    }
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
