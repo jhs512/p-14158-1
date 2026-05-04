e2f197fbe0b15480477675c9110b52d560ba7665
jhs512 <jangka512@gmail.com>
2026-05-04 15:25:24 +0900

    019

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -7,6 +7,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration
 public class BaseInitData {
     @Autowired
@@ -15,12 +17,22 @@ public class BaseInitData {
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            if (postRepository.count() > 0) return;
-
-            Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
-            Post post2 = postRepository.save(new Post("제목 2", "내용 2"));
-
-            System.out.println("기본 데이터가 초기화되었습니다.");
+            work1();
+            work2();
         };
+    }
+
+    void work1() {
+        if (postRepository.count() > 0) return;
+
+        Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+        Post post2 = postRepository.save(new Post("제목 1", "내용 2"));
+
+        System.out.println("기본 데이터가 초기화되었습니다.");
+    }
+
+    void work2() {
+        Optional<Post> opPost1 = postRepository.findById(1);
+        // SELECT * FROM post WHERE id = 1;
     }
 }
