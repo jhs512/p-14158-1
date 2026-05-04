6bf42c9b8642f1a03e49f3aa2b1c8190c94deabd
jhs512 <jangka512@gmail.com>
2026-05-04 15:37:03 +0900

    023

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -15,21 +15,17 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private final int id;
+    private int id;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
-        this.id = 0;
         this.title = title;
         this.content = content;
-    }
-
-    public Post() {
-        this(null, null);
     }
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -25,8 +25,12 @@ public class BaseInitData {
     void work1() {
         if (postRepository.count() > 0) return;
 
-        Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+        Post post1 = new Post("제목 1", "내용 1");
+        postRepository.save(post1);
         Post post2 = postRepository.save(new Post("제목 1", "내용 2"));
+
+        System.out.println(post1.getId());
+        System.out.println(post2.getId());
 
         System.out.println("기본 데이터가 초기화되었습니다.");
     }
